7aa9e9e64e974f447d7b626d1836ead5583c4de3
Bryan Clover <bclover09@gmail.com>
2016-10-10 11:18:25 -0400

    Added basic form validation.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -15,16 +15,25 @@ class PostsNew extends Component {
         <div className="form-group">
           <label>Title</label>
           <input type="text" className="form-control" { ...title } />
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div> 
         
         <div className="form-group">
           <label>Categories</label>
           <input type="text" className="form-control" { ...categories } />
+          <div className="text-help">
+            {categories.touched ? categories.error : '' }
+          </div>
         </div> 
         
         <div className="form-group">
           <label>Content</label>
           <textarea  className="form-control" { ...content } />
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
         </div> 
         
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -34,7 +43,26 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if(!values.title) {
+    errors.title = 'Please enter a username.';
+  }
+
+  if(!values.categories) {
+    errors.categories = 'Please enter a category.';
+  }
+  
+  if(!values.content) {
+    errors.content = 'Please enter some content.';
+  }
+
+  return errors;
+}
+
 export default reduxForm({ 
   form: 'PostsNewForm',
-  fields: [ 'title', 'categories', 'content']
+  fields: [ 'title', 'categories', 'content'],
+  validate
 }, null, { createPost })(PostsNew);
