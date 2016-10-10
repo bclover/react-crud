455e02e9ca9874f30cdb3d6a769c49a2b1b13aaa
Bryan Clover <bclover09@gmail.com>
2016-10-10 16:34:17 -0400

    Built list of posts.

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -8,6 +8,17 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+        </li>
+      );
+    });
+  }
+
   render() {
     return  (
       <div>
@@ -16,11 +27,18 @@ class PostsIndex extends Component {
             Add A Post
           </Link>
         </div>
-        List of blog posts.
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   } 
 }
 
-export default connect(null, { fetchPosts } )(PostsIndex);
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
+}
+
+export default connect(mapStateToProps, { fetchPosts } )(PostsIndex);
 
