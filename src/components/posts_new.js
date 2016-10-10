11b3c4e8aa463b1e91f06d478038e89030b7d006
Bryan Clover <bclover09@gmail.com>
2016-10-10 13:02:07 -0400

    Added form validation and cancel button.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
   render() {
@@ -12,32 +13,32 @@ class PostsNew extends Component {
     return (
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a New Post</h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" { ...title } />
-          <div className="text-help">
+          <div className="text-danger">
             {title.touched ? title.error : ''}
           </div>
         </div> 
         
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" { ...categories } />
-          <div className="text-help">
+          <div className="text-danger">
             {categories.touched ? categories.error : '' }
           </div>
         </div> 
         
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea  className="form-control" { ...content } />
-          <div className="text-help">
+          <div className="text-danger">
             {content.touched ? content.error : ''}
           </div>
         </div> 
         
         <button type="submit" className="btn btn-primary">Submit</button>
-
+        <Link id="cancel-button"  to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
