be8a9fb27a74dfe10eb8161228dbabc69e070754
Bryan Clover <bclover09@gmail.com>
2016-10-13 10:40:58 -0400

    Continued wiring up a request for a single blog post.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
+export const FETCH_POST = "FETCH_POST";
 export const FETCH_POSTS = "FETCH_POSTS";
 export const CREATE_POST = "CREATE_POST";
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=bclover16";
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  };
+}
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts/${API_KEY}`);
